f9ccb94da28e3187da78b0c8d5d55f94ff67bf71
Tamim Sarkar <2010097@me.buet.ac.bd>
2022-06-16 21:38:41 +0600

    16-6-2022

--- notice/admin.py
+++ notice/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from notice.models import Person, Category, School, Notice
+
 
 # Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Category)
+admin.site.register(School)
+admin.site.register(Notice)
--- notice/models.py
+++ notice/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
+
+
+class Person(models.Model):
+    name = models.CharField(max_length = 100)
+    age = models.CharField(max_length = 50, blank = True)
+    dateOfBirth = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Category(models.Model):
+    title = models.CharField(max_length = 100)
+    class_teacher = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.title
+    
+
+class Notice(models.Model):
+    person = models.ForeignKey("Person",  on_delete=models.CASCADE)
+    description = models.TextField(max_length = 500)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.description
+    
+
+class School(models.Model):
+    school_name =models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.school_name
+    
+
+
+
+
+
 # Create your models here.
+
+# python manage.py migrate
+# python manage.py makemigrations
