e1538c5d2523c8ce646d94b23073593bd049df7e
Tamim Sarkar <2010097@me.buet.ac.bd>
2022-06-17 20:37:47 +0600

    17 06

--- notice/admin.py
+++ notice/admin.py
@@ -4,7 +4,29 @@ from notice.models import Person, Category, School, Notice
 
 # Register your models here.
 
-admin.site.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("name", "age", "dateOfBirth") # change korte hobe eituk
+
+    def active(self, obj):
+        return obj.is_active == 1
+    
+    active.boolean = True
+
+    
+    def has_delete_permission(self, request, obj = None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Category)
 admin.site.register(School)
 admin.site.register(Notice)
+
+# Customization
+
+admin.site.site_header = "NoticeBoard Admin Panel"
+admin.site.site_title = "Admin Portal Sourav"
+admin.site.index_title = "Hello and welcome to"
+
